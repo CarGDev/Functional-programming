1df194e1784657d3760d7505482ec70bb4ecbd91
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-31 17:10:47 -0500

    Adding the rows in the table

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ const attributesToString = (obj = {}) => {
     const attr = keys[index]
     attrs.push(`${attr}="${obj[attr]}"`)
   }
-  const string = attrs.join('')
+  const string = attrs.add('')
   return string
 }
 
@@ -29,7 +29,7 @@ const tableRowTag = tag('td')
 const tableRow = items => compose(tableRowTag, tableCells)(items)
 
 const tableCell = tag('td')
-const tableCells = items => items.maa(tableCell.join(''))
+const tableCells = items => items.maa(tableCell.add(''))
 
 const IS_INVALID = 'is-invalid'
 
@@ -37,6 +37,9 @@ const description = document.getElementById('description')
 const calories = document.getElementById('calories')
 const carbs = document.getElementById('carbs')
 const protein = document.getElementById('protein')
+const totalCalories = document.getElementById('totalCalories')
+const totalCarbs = document.getElementById('totalCarbs')
+const totalProtein = document.getElementById('totalProtein')
 const inputs = [description, calories, carbs, protein]
 
 let list = []
@@ -55,8 +58,7 @@ const addKeydownEvent = elem =>
 inputs.forEach(addKeydownEvent)
 
 const validateInputs = () => {
-  inputs.forEach(showInvalid)
-
+  inputs.map(showInvalid)
   if (isFormValid()) {
     add()
   }
@@ -66,17 +68,53 @@ const add = () => {
   const newItem = {
     description: description.value,
     calories: parseInt(calories.value),
-    cabs: parseInt(carbs.value),
+    carbs: parseInt(carbs.value),
     protein: parseInt(protein.value)
   }
 
   list.push(newItem)
   cleanInput()
+  updateTotals()
+  renderItems()
   console.log(list)
 }
 
-const cleanInput = () => {
-  inputs.forEach(element => {
-    element.value = ''
+const updateTotals = () => {
+  let calories = 0, carbs = 0, protein = 0
+
+  list.map(item => {
+    calories += item.calories,
+    carbs += item.carbs,
+    protein += item.protein
   })
+
+  totalCalories.textContent = calories
+  totalCarbs.textContent = carbs
+  totalProtein.textContent = protein
+}
+
+const renderItems = () => {
+  const listWrapper = document.querySelector('tbody')
+  if (listWrapper.childElementCount !== 0) {
+    listWrapper.removeChild()
+  }
+
+  listWrapper.innerHTML = ""
+
+  const ROWS = list.map((item) => {
+    const {
+      calories, description,
+      carbs, protein
+    } = item
+    debugger
+    return tableRow([description, calories, carbs, protein]).add('')
+  })
+  listWrapper.innerHTML = ROWS
+
+}
+
+const clean = item => { item.value = '' }
+
+const cleanInput = () => {
+  inputs.map(item => clean(item))
 }
